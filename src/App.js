ebc5b0eeb08a2e040a50642877c421ddb2186239
Danail Videv <danailvidev@gmail.com>
2018-08-22 13:26:20 +0300

    update packages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as _ from 'lodash';
 import './App.css';
 
-class App extends Component {
-  render() {
+const Stars = (props) => {
+  const numberOfStars = Math.floor(Math.random()*9) + 1;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React2</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="col-5">
+        {_.range(numberOfStars).map(i => 
+          <i key={i} className="fa fa-star"></i>
+        )}
       </div>
-    );
+    )
   }
-}
+  
+  const Button = (props) => {
+    return (
+      <div className="col-2">
+        <button>=</button>
+      </div>
+    )
+  }
+  
+  const Answer = (props) => {
+    return (
+      <div className="col-5">
+        <span>6</span>
+      </div>
+    )
+  }
+  
+  const Numbers = (props) => {
+    return (
+      <div className="card text-center">
+        <div>
+          {Numbers.list.map((number, i) => 
+            <span key={i}>{number}</span>
+          )}
+        </div>
+      </div>
+    )
+  }
+  Numbers.list = _.range(1,10);
+  
+  class Game extends Component {
+    render() {
+      return (
+          <div  className="container">
+            <h3>Play Nine</h3>
+            <hr/>
+            <div className="row">
+              <Stars />
+              <Button />
+              <Answer />
+            </div>
+            <br />
+            <Numbers />
+          </div>
+        )
+      }
+  }
+  
+  class App extends Component {
+    render() {
+      return (
+          <div>
+            <Game />
+          </div>
+        )
+      }
+  }
 
 export default App;
